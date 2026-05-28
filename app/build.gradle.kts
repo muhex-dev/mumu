20c728c06a0c75061cda2b650c9982c96a6fd2da
muhex <muhexx1@gmail.com>
2026-05-28 22:53:25 +0300

    Refactor: Rename project-wide package from com.example.myapplication to com.muhex.mumu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.myapplication"
+    namespace = "com.muhex.mumu"
     compileSdk = 35
 
     buildFeatures {
@@ -14,7 +14,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "com.muhex.mumu"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
